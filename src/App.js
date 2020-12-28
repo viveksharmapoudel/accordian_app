069679203937d1731432df4fdc20537105deba4d
vivek <viveksharmapoudel@gmail.com>
2020-12-28 12:02:50 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import questions from './data';
+import Question from './Question';
 function App() {
+  
+  console.log(questions)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <main>
+          <div className="container">
+              <h3>Questions And Answers About Login </h3>
+            <section className="info">
+                  {
+                    questions.map(singleQuestion =>{
+                      return <Question  {...singleQuestion} />
+                      })
+                  }
+            </section>
+          </div>
+        </main>
+  )
 }
 
 export default App;
